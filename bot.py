86889d73a909c2d8ea25894eb30e8ec9fd8a549f
Sepehr AK <sepehrakmd@gmail.com>
2025-11-25 18:34:41 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+from telegram.ext import Updater, CommandHandler
 
+TOKEN = "8468267497:AAHWMeqnKuQwKDC-CWHVZcEZMpegVxzxA9A"
+
+def start(update, context):
+    update.message.reply_text(
+        "سلام! 👋\n"
+        "من ربات مداستریم هستم.\n"
+        "برای شروع /start رو زدی و آماده‌ام!"
+    )
+
+def main():
+    updater = Updater(TOKEN, use_context=True, request_kwargs={'read_timeout': 20, 'connect_timeout': 20})
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    print("بات در حال اجراست...")
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
